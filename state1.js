64335d259afda4af291ef78e8378d7ff4f791cca
Jorge Salgado <salgado.jorge@dreyfous.local>
2016-08-11 11:26:10 -0400

    first commit

---
+++ state1.js
@@ -0,0 +1,9 @@
+var demo = {};
+demo.state1 = function(){};
+demo.state1.prototype = {
+    preload: function(){},
+    create: function(){
+        game.stage.backgroundColor = '#DDDDDDD';
+    },
+    update: function(){},
+};
